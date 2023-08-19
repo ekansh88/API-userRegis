dab3d23dc6aa74b0ba56eb6b76ce9bdf01604f0d
Ekansh Sharma <106429249+ekansh88@users.noreply.github.com>
2023-08-19 17:37:44 +0530

    task 2

--- models/userReg.js
+++ models/userReg.js
@@ -17,7 +17,50 @@ const userSchema = mongoose.Schema(
     }
 
 )
+// new post by the email that is registered
+const userPost = mongoose.Schema(
+    {
+        email:{
+            type:String,
+            require: [true, "enter id"]
+        },
+        path:{
+            type:String,
+            require: [true, "enter id"]
+        }
+    },
+    {
+        timestamps: true 
+    }
+
+)
+
+// incresing the like and adding comment in the post
+const postLikeComment = mongoose.Schema(
+    {
+        postId:{
+            type:String,
+            require: [true, "enter id"]
+        },
+        like:{
+            type:Number,
+            require: [true, "enter id"]
+        },
+        comment:{
+            type:String,
+            require: [true, "enter id"]
+        }
+    },
+    {
+        timestamps: true 
+    }
+
+)
 
 const User = mongoose.model("User", userSchema);
+const Post = mongoose.model("Post", userPost);
+const PostLikeComment = mongoose.model("PostLikeComment", postLikeComment);
 
 module.exports = User;
+module.exports = Post;
+module.exports = PostLikeComment;
